8f00691353839157ec2b4384eeae67dcb75b2af1
iliyachaaban <ichaaban.ieu2023@student.ie.edu>
2025-03-26 18:03:54 +0100

    Finish Poker Game

--- Poker_Game.py
+++ Poker_Game.py
@@ -17,16 +17,60 @@ class PokerHand:
         first_suit = self.cards[0].suit  # Get the suit of the first card
         return all(card.suit == first_suit for card in self.cards)  # Check all cards
 
-# Simulation to estimate the probability of a flush
-count = 0
-flushes = 0
+    @property
+    def is_full_house(self):
+        return self.number_matches == 8
 
-while flushes < 100:  # Run until 100 flush hands are found
+    @property
+    def number_matches(self):
+        matches = 0
+        for i in range(len(self.cards)):
+            for j in range(len(self.cards)):
+                if i == j:
+                    continue
+                if self.cards[i].rank == self.cards[j].rank:
+                    matches += 1
+        return matches
+
+    @property
+    def is_pair(self):
+        if self.number_matches == 2: #simple way
+            return True
+        return False
+
+
+    @property
+    def is_two_pair(self):
+        return self.number_matches == 4 #more advanced!
+
+    @property
+    def is_trips(self):
+        if self.number_matches == 6:
+            return True
+        return False
+
+    @property
+    def is_quads(self):
+        if self.number_matches == 12:
+            return True
+        return False
+
+    @property
+    def is_straight(self):
+        self.cards.sort()
+        distance = Card.RANKS.index(self.cards[4].rank) - \
+                   Card.RANKS.indeex(self.cards[0])
+        return self.number_matches == 0 and distance == 4
+
+count = 0
+matches = 0
+while matches < 10:
     deck = Deck()
     deck.shuffle()
     hand = PokerHand(deck)
-    if hand.is_flush:
-        flushes += 1
+    if hand.is_full_house:
+        matches += 1
+        print(hand)
     count += 1
 
-print(f"Probability of a flush is {100 * flushes / count:.2f}%")
+print(f"Probability of a straight is {100*matches/count}%")
